8ba66adbd625cb08c201d5054a81d70dbff7e83e
Vaibhav Singh <singhvaibhav654@gmail.com>
2024-04-05 00:28:31 +0530

    first commit

---
+++ src/pg.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import pg from './33025771_7990317.jpg'
+
+export default function Pg() {
+  return (
+    <section className="text-gray-600 body-font overflow-hidden">
+  <div className="container px-5 py-24 mx-auto">
+    <div className="lg:w-4/5 mx-auto flex flex-wrap">
+      <img alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src={pg}/>
+      <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+        <h2 className="text-sm font-semibold text-gray-500 tracking-widest">Codeiox</h2>
+        <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">Payment Page for Contribution!</h1>
+        <div className="flex mb-4">
+        
+        </div>
+        <p className="leading-relaxed">
+        We warmly invite you to make a meaningful contribution starting at Rs 99 towards Codeiox. Your generosity holds the power to create a significant impact! Should you desire to contribute more, the choice is entirely yours. Together, let's embark on this journey to make a positive difference!
+        </p>
+        <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
+          <div className="flex">
+               </div>
+          <div className="flex ml-6 items-center">
+          
+            <div className="relative">
+              
+              <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
+                
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="flex">
+          <span className="title-font font-medium text-2xl text-gray-900">₹99.00</span>
+          <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded" style={{backgroundColor:'rgb(210,104,204)'}}><a href='https://www.instamojo.com/@rccodex/l8fd5d074ecbd49a19dd11e7efd642f21/'> Payment</a></button>
+         
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+  )
+}
